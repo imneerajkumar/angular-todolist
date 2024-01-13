c95821f92023bf7d568b563890d982870f987bf4
Neeraj Kumar <nijjukr@gmail.com>
2024-01-14 00:32:08 +0530

    Routing added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
-import { AddItemComponent } from './add-item/add-item.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [CommonModule, RouterOutlet, TodosComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, TodosComponent],
 })
 export class AppComponent {
   title = 'angulrar-tut';

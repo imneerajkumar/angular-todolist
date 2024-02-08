b78c1d59add21d707c76d492bdee655a90a3d2b4
Neeraj Kumar <imneerajkumar23@gmail.com>
2024-02-08 17:06:59 +0530

    Api requests

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, HttpClientModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
-export class AboutComponent {}
+export class AboutComponent implements OnInit {
+  users: any = [];
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http
+      .get('https://jsonplaceholder.typicode.com/users')
+      .subscribe((res) => {
+        this.users = res;
+      });
+  }
+
+  postUser() {
+    this.http
+      .post('https://jsonplaceholder.typicode.com/posts', {
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+      })
+      .subscribe((res) => console.log(res));
+  }
+}
